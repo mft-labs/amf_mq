2beb7458c197a3e7b228233b2dcee762d0511b4b
madhu72 <madhu@hub4edi.com>
2022-04-19 17:04:39 +0530

    Fix invalid subscription error with nats

--- nats2.go
+++ nats2.go
@@ -117,13 +117,47 @@ func (nmq *NMQType2) Put(qname string, data []byte) (err error) {
 	return err
 }
 
+func (nmq *NMQType2) ReSubscribe() error {
+	var err error
+	if nmq.sub != nil {
+		nmq.sub.Unsubscribe()
+		nmq.sub.Drain()
+	}
+	nmq.sub, err = nmq.js2.SubscribeSync(nmq.inputq+".*", nats.Durable("monitor"), nats.MaxDeliver(1))
+	if err!=nil && strings.Contains(err.Error(),"nats: no stream matches subject") {
+		nmq.js2.AddStream(&nats.StreamConfig{
+			Name:     nmq.inputq,
+			Subjects: []string{nmq.inputq+".*"},
+		})
+
+		nmq.sub, err = nmq.js2.SubscribeSync(nmq.inputq+".*", nats.Durable("monitor"), nats.MaxDeliver(1))
+	}
+	if err!=nil && strings.Contains(err.Error(),"consumer is already bound to a subscription") {
+		return nil
+	}
+	return err
+}
+
 func (nmq *NMQType2) Get(wait int64) (data []byte, err error) {
 	msg, err := nmq.sub.NextMsg(time.Second*time.Duration(wait))
 	if err!=nil {
-		if strings.Contains(err.Error(),"nats: timeout") {
+		/*if strings.Contains(err.Error(),"nats: timeout") {
 			return nil, nil
 		}
-		return nil, err
+		return nil, err*/
+		if strings.Contains(err.Error(),"nats: timeout") || strings.Contains(err.Error(),"nats: invalid subscription")  {
+			fmt.Printf("Error received:%v\n",err)
+			if strings.Contains(err.Error(),"invalid subscription") {
+				err = nmq.ReSubscribe()
+				if err==nil {
+					fmt.Printf("Retrying to get message")
+					msg, err = nmq.sub.NextMsg(time.Second*time.Duration(wait))
+				}
+			} else {
+				return nil, nil
+			}
+
+		}
 	}
 	nmq.msg = msg
 	//nmq.con2.Publish(msg.Reply,[]byte("Received"))
